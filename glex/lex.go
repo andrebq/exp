fb2a5eec472c0a98a75a5eef3b5a8f8133a57e36
André Moraes <andre@amoraes.info>
2012-11-18 17:21:47 -0200

    More testing and gofmt

--- glex/lex.go
+++ glex/lex.go
@@ -7,21 +7,23 @@ import (
 
 // Lexer errors
 type Error string
+
 // Error interface
 func (e Error) Error() string { return string(e) }
+
 // Stringer interface
 func (e Error) String() string { return string(e) }
 
 const (
 	NoMatch = Error("No match was found in the list")
-	Eof = Error("End of input")
+	Eof     = Error("End of input")
 )
 
 // Interface that define's a Match
 type Match struct {
 	// text of the match
 	Text string
-	
+
 	// token number
 	Token int
 }
@@ -36,7 +38,7 @@ var (
 type Rule struct {
 	// regular expression for the given match
 	exp *regexp.Regexp
-	
+
 	// Token for the given matches
 	token int
 }
@@ -60,17 +62,18 @@ func NewRule(re string, token int) (*Rule, error) {
 //
 // On any other cases, this will return a Match, the rest of the string and nil.
 func (r *Rule) Match(input string) (Match, string, error) {
-	if len(input) == 0 { return emptyMatch, input, Eof }
-	
+	if len(input) == 0 {
+		return emptyMatch, input, Eof
+	}
+
 	loc := r.exp.FindStringIndex(input)
 	if loc == nil {
 		return emptyMatch, input, NoMatch
 	}
-	
-	m := Match{Text:input[loc[0]:loc[1]], Token: r.token}
+
+	m := Match{Text: input[loc[0]:loc[1]], Token: r.token}
 	return m, input[loc[1]:], nil
 }
-
 
 // Hold the list of rules
 type RuleList []*Rule
@@ -79,14 +82,30 @@ type RuleList []*Rule
 //
 // This method simply call's the Match method of each Rule in the list.
 func (l RuleList) Match(input string) (Match, string, error) {
-	if len(input) == 0 { return emptyMatch, input, Eof }
-	
-	for _, r := range(l) {
+	if len(input) == 0 {
+		return emptyMatch, input, Eof
+	}
+
+	for _, r := range l {
 		m, tail, err := r.Match(input)
-		if err != nil {
+		if err == nil {
 			return m, tail, err
 		}
 	}
-	
+
 	return emptyMatch, input, NoMatch
+}
+
+// Include the given expression/token pair into this
+// rule list.
+//
+// If there are erros in the epxression, the rule isn't
+// included and the error is returned.
+func (l *RuleList) NewRule(exp string, token int) (*Rule, error) {
+	r, err := NewRule(exp, token)
+	if err != nil {
+		return nil, err
+	}
+	*l = append(*l, r)
+	return r, nil
 }
--- glex/lex_test.go
+++ glex/lex_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestRule(t *testing.T) {
-	r, err := NewRule("abba",1)
+	r, err := NewRule("abba", 1)
 	if err != nil {
 		t.Fatalf("Error compiling regexp. %v", err)
 	}
@@ -14,7 +14,7 @@ func TestRule(t *testing.T) {
 	if err != nil {
 		t.Errorf("Unexpected error: %v", err)
 	}
-	
+
 	if m.Token != 1 {
 		t.Errorf("Expecting token value of %v got %v", 1, m.Token)
 	}
@@ -25,5 +25,32 @@ func TestRule(t *testing.T) {
 
 	if input != " c abba" {
 		t.Errorf("Invalid tail, should be %v got %v", " c abba", input)
+	}
+}
+
+func TestRuleList(t *testing.T) {
+	rl := make(RuleList, 0)
+	_, err := rl.NewRule("abba", 1)
+	if err != nil {
+		t.Fatalf("Error compiling expression. %v", err)
+	}
+	_, err = rl.NewRule("cba", 2)
+	if err != nil {
+		t.Fatalf("Erro compiling expression. %v", err)
+	}
+	input := "abbacba"
+	m, input, err := rl.Match(input)
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+	if m.Text != "abba" {
+		t.Errorf("Expecting %v got %v", "abba", m.Text)
+	}
+	m, input, err = rl.Match(input)
+	if err != nil {
+		t.Fatalf("Unexpected error %v", err)
+	}
+	if m.Text != "cba" {
+		t.Errorf("Expecting %v got %v", "cba", m.Text)
 	}
 }
