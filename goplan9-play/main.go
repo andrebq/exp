33e7cf3945a2994117a1a0a6cc6113d475c69496
André Moraes <andre@amoraes.info>
2013-04-20 17:58:39 -0300

    Tattach, Tversion, Twalk

--- goplan9-play/main.go
+++ goplan9-play/main.go
@@ -4,7 +4,10 @@ import (
 	"code.google.com/p/goplan9/plan9"
 	"net"
 	"flag"
+	"fmt"
+	"io"
 	"log"
+	"sync"
 )
 
 var (
@@ -14,13 +17,15 @@ var (
 
 type Server struct {
 	listener net.Listener
+	explorer FileExplorer
 }
 
-func NewServer(lnet, laddr string) (*Server, error) {
+func NewServer(lnet, laddr string, explorer FileExplorer) (*Server, error) {
 	s := &Server{}
 	l, err := net.Listen(lnet, laddr)
 	if err != nil { return nil, err }
 	s.listener = l
+	s.explorer = explorer
 	return s, nil
 }
 
@@ -36,25 +41,286 @@ func (s *Server) Start() error {
 			println("error", err)
 			return err
 		}
-		go s.handleClient(client)
+		cc := NewClientConn(client, s, s.explorer)
+		go cc.handle()
 	}
 	return nil
 }
 
 func (s *Server) handleError(err error, c net.Conn) {
-	log.Printf("Client %v caused error %v", c, err)
+	log.Printf("Client %v caused error %v", c.RemoteAddr(), err)
 }
 
-func (s *Server) handleClient(c net.Conn) {
-	defer c.Close()
+func readFCall(out chan *plan9.Fcall, done chan signal, err chan error, input io.Reader) {
+loop:
 	for {
-		fcall, err := plan9.ReadFcall(c)
-		if err != nil {
-			s.handleError(err, c)
-			break
+		select {
+		case <-done:
+			close(out)
+			break loop
+		default:
+			fc, e := plan9.ReadFcall(input)
+			if e != nil {
+				err <- e
+				close(out)
+				break
+			}
+			out <- fc
 		}
-		println(fcall.String())
 	}
+}
+
+func writeFCall(out io.Writer, done chan signal, err chan error, input chan *plan9.Fcall) {
+loop:
+	for {
+		select {
+		case <-done:
+			break loop
+		case fc := <-input:
+			if fc == nil {
+				continue
+			}
+			e := plan9.WriteFcall(out, fc)
+			if e != nil {
+				err <- e
+				break
+			}
+		}
+	}
+}
+
+// Handle all client interaction
+type ClientConn struct {
+	sync.RWMutex
+	// remote connection
+	conn net.Conn
+	// server
+	server *Server
+	// map between path's and fileRef's
+	fileRefs map[uint64]*fileRef
+	// map between a fid and a qid
+	fidmap map[uint32]uint64
+	// explorer used to navigate the FS
+	explorer FileExplorer
+}
+
+// Interface used to navigate, open and close files in the system
+type FileExplorer interface {
+	// Must return the unique identifier for this explorer root.
+	//
+	// The returned value can't be zero, since 0 is considered a non-existing path 
+	Root() uint64
+	// Should return the unique identifier and type of the given name under the given directory
+	//
+	// If the name don't exists under parent, should return 0
+	Walk(parent uint64, name string) (uint64, FileType)
+}
+
+// Represent a reference to a file
+type fileRef struct {
+	plan9.Qid
+}
+
+// Type of a file
+type FileType uint8
+
+const (
+	FTFILE = FileType(plan9.QTFILE)
+	FTDIR = FileType(plan9.QTDIR)
+	FTMOUNT = FileType(plan9.QTMOUNT)
+)
+
+func NewClientConn(conn net.Conn, server *Server, explorer FileExplorer) *ClientConn {
+	cc := &ClientConn{conn: conn, server: server, fileRefs: make(map[uint64]*fileRef),
+		explorer: explorer, fidmap: make(map[uint32]uint64)};
+	return cc
+}
+
+// used to send signal's between goroutines
+type signal struct{}
+
+func (c *ClientConn) handle() {
+	defer c.Close()
+	input := make(chan *plan9.Fcall)
+	output := make(chan *plan9.Fcall)
+	done := make(chan signal)
+	err := make(chan error)
+
+	go readFCall(input, done, err, c.conn)
+	go writeFCall(c.conn, done, err, output)
+loop:
+	for {
+		select {
+		case fc := <-input:
+			go c.process(fc, output)
+		case e := <-err:
+			c.server.handleError(e, c.conn)
+			done <- signal{}
+			done <- signal{}
+			break loop
+		}
+	}
+}
+
+func (c *ClientConn) process(fc *plan9.Fcall, out chan *plan9.Fcall) {
+	println(">>>\t", fc.String())
+	switch fc.Type {
+	case plan9.Tversion:
+		fc = c.version(fc)
+	case plan9.Tattach:
+		fc = c.attach(fc)
+	case plan9.Twalk:
+		fc = c.walk(fc)
+	default:
+		println("!!!\t", fc.String())
+		fc = nil
+	}
+	if fc != nil {
+		println("<<<\t", fc.String())
+	}
+	out <- fc
+}
+
+func (c *ClientConn) version(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rversion
+	fc.Version = "9P2000"
+	return fc
+}
+
+func (c *ClientConn) attach(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rattach
+	fref, _ := c.createFileRef(c.explorer.Root(), FTMOUNT, 0)
+	fc.Qid = fref.Qid
+	c.bindFid(fc.Fid, fc.Qid.Path)
+	return fc
+}
+
+func (c *ClientConn) walk(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rwalk
+	fref, has := c.fidRef(fc.Fid)
+	if !has {
+		return c.invalidFidErr(fc)
+	}
+	if _, has := c.fidRef(fc.Newfid); has {
+		return c.fidUsedErr(fc)
+	}
+	current := fref.Path
+	for idx, name := range fc.Wname {
+		var ft FileType
+		current, ft = c.explorer.Walk(current, name)
+		if current == 0 {
+			return c.fileNotFoundErr(fc)
+		}
+		ref, err := c.createFileRef(current, ft, 0)
+		if err != nil {
+			return c.unexpectedErr(fc, err)
+		}
+		fc.Wqid = append(fc.Wqid, ref.Qid)
+
+		// if the last match isn't a directory, there is no need to find
+		// another part of the path
+		//
+		// so, just break here
+		if ft == FTFILE && idx != len(fc.Wname) - 1 {
+			println("here")
+			return c.fileNotFoundErr(fc)
+		}
+	}
+	return fc
+}
+
+func (c *ClientConn) invalidFidErr(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rerror
+	fc.Ename = "fid not found"
+	return fc
+}
+
+func (c *ClientConn) fileNotFoundErr(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rerror
+	fc.Ename = "file not found"
+	return fc
+}
+
+func (c *ClientConn) fidUsedErr(fc *plan9.Fcall) *plan9.Fcall {
+	fc.Type = plan9.Rerror
+	fc.Ename = "fid in use"
+	return fc
+}
+
+func (c *ClientConn) unexpectedErr(fc *plan9.Fcall, err error) *plan9.Fcall {
+	fc.Type = plan9.Rerror
+	fc.Ename = err.Error()
+	return fc
+}
+
+// Return the file referenced by the given fid
+func (c *ClientConn) fidRef(fid uint32) (*fileRef, bool) {
+	c.RLock()
+	defer c.RUnlock()
+	if qid, has := c.fidmap[fid]; has {
+		fref, has := c.fileRefs[qid]
+		return fref, has
+	}
+	return nil, false
+}
+
+// Bind the given fid to the provided path
+//
+// if path is 0, remove the fid
+func (c *ClientConn) bindFid(fid uint32, path uint64) {
+	c.Lock()
+	defer c.Unlock()
+	_, has := c.fidmap[fid]
+	if path == 0 && has {
+		delete(c.fidmap, fid)
+		return
+	}
+	c.fidmap[fid] = path
+}
+
+// Create a file for the given path, if the path is already present, then just check 
+// if the version/type are the same, if they are, just return the existing file, otherwise
+// return an error
+//
+// If the version is 0, then this check is ignored and any version existing on the server is returned
+//
+// Every new resources created will have a version of 1 instead of 0.
+func (c *ClientConn) createFileRef(path uint64, ft FileType, version uint32) (*fileRef, error) {
+	c.Lock()
+	defer c.Unlock()
+	if fref, has := c.fileRefs[path]; has {
+		if fref.Path == path && (fref.Vers == version || version == 0) && uint8(ft) == fref.Type {
+			return fref, nil
+		}
+		return fref, fmt.Errorf("path %v already used", path)
+	}
+	fref := &fileRef{}
+	if version == 0 {
+		version = 1
+	}
+	fref.Type = uint8(ft)
+	fref.Vers = version
+	fref.Path = path
+	c.fileRefs[path] = fref
+	return fref, nil
+}
+
+func (c *ClientConn) Close() error {
+	return c.conn.Close()
+}
+
+type dummyExplorer struct{}
+
+func (d dummyExplorer) Root() uint64 {
+	return 1
+}
+
+func (d dummyExplorer) Walk(parent uint64, name string) (uint64, FileType) {
+	println("\t...searching ", parent, " for file: ", name)
+	if parent == 1 && name == "dummy" {
+		return 2, FTFILE
+	}
+	return 0, FTFILE
 }
 
 func main() {
@@ -63,7 +329,7 @@ func main() {
 		flag.Usage()
 		return
 	}
-	server,err := NewServer("tcp", *listen)
+	server,err := NewServer("tcp", *listen, dummyExplorer{})
 	if err != nil {
 		log.Fatalf("Unable to create server. Cause: %v", err)
 	}
