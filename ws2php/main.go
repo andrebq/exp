d99ac035ba19dc6c93fca637d6710d018d020f97
Andre Moraes <andre@amoraes.info>
2013-09-03 09:02:10 -0300

    broker plumbing almost done, pending session

--- ws2php/main.go
+++ ws2php/main.go
@@ -4,8 +4,13 @@
 package main
 
 import (
-	"net/http"
 	"code.google.com/p/go.net/websocket"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
 )
 
 type signal struct{}
@@ -14,15 +19,18 @@ type method func(*Broker) (interface{}, error)
 
 type brokerMessage struct {
 	request method
-	completed chan signal
-	err error
-	data interface{}
+	done    chan signal
+	err     error
+	data    interface{}
 }
 
 // Hold all websocket sessions that are running at this moment
 type Broker struct {
-	list []*Session
-	done chan signal
+	list       []*Session
+	done       chan signal
+	message    chan *brokerMessage
+	lastSid    uint64
+	backendUrl *url.URL
 }
 
 func NewBroker() *Broker {
@@ -30,17 +38,34 @@ func NewBroker() *Broker {
 		list: make([]*Session, 0),
 	}
 	go s.loop()
+	return s
 }
 
-func (s *Broker) CreateSession(cli websocket.Conn) (*Session, error) {
+func (s *Broker) openSessionWithBackend(newSession *Session) error {
+	return nil
+}
+
+func (s *Broker) CreateSession(cli *websocket.Conn) (*Session, error) {
+	m := s.createMessage(func(b *Broker) (interface{}, error) {
+		s.lastSid++
+		newSession := &Session{
+			Cid:     s.lastSid,
+			Backend: s.backendUrl,
+		}
+		err := s.openSessionWithBackend(newSession)
+		return newSession, err
+	})
+	s.message <- m
+	<-m.done
+	return m.data.(*Session), m.err
 }
 
 func (s *Broker) createMessage(m method) *brokerMessage {
-	ret := brokerMessage {
+	ret := &brokerMessage{
 		request: m,
-		completed: make(chan signal, 0),
-		err: nil,
-		data: nil,
+		done:    make(chan signal, 0),
+		err:     nil,
+		data:    nil,
 	}
 	return ret
 }
@@ -51,8 +76,25 @@ loop:
 		select {
 		case <-s.done:
 			break loop
+		case m := <-s.message:
+			s.processMessage(m)
 		}
 	}
+}
+
+func (s *Broker) processMessage(m *brokerMessage) {
+	defer func(m *brokerMessage) {
+		if err := recover(); err != nil {
+			switch err := err.(type) {
+			case error:
+				m.err = err
+			default:
+				m.err = fmt.Errorf("Error: %v", err)
+			}
+		}
+		go func(m *brokerMessage) { m.done <- signal{} }(m)
+	}(m)
+	m.data, m.err = m.request(s)
 }
 
 // Session between the websocket and the backend
@@ -64,23 +106,32 @@ type Session struct {
 	Backend *url.URL
 }
 
-var broker = NewBroker()
+var (
+	broker = NewBroker()
+	port   = flag.String("p", ":8081", "Port to listen for websocket connections")
+	help   = flag.Bool("h", false, "Show this menu")
+)
 
-func handleWebSocket(conn websocket.Conn) {
-	session := broker.CreateSession(conn)
-loop:
-	for {
-		select {
-		case data := <-session.DataFromBackend:
-			session.WriteClient(data)
-		case data := <-session.DataFromClient:
-			session.WriteBackend(data)
-		case <-session.Done:
-			defer session.CloseClient()
-			defer session.CloseBackend()
-			break loop
-		}
+func handleWebSocket(conn *websocket.Conn) {
+	session, err := broker.CreateSession(conn)
+	_ = session
+	if err != nil {
+		log.Printf("Error creating session: %v", err)
+		defer conn.Close()
 	}
+	//loop:
+	//	for {
+	//		select {
+	//		case data := <-session.DataFromBackend:
+	//			session.WriteClient(data)
+	//		case data := <-session.DataFromClient:
+	//			session.WriteBackend(data)
+	//		case <-session.Done:
+	//			defer session.CloseClient()
+	//			defer session.CloseBackend()
+	//			break loop
+	//		}
+	//	}
 }
 
 func main() {
@@ -96,4 +147,3 @@ func main() {
 		log.Fatalf("Error: %v", err)
 	}
 }
-
