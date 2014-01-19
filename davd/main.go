590aebfba07ea0bc0d3b29b9796c76d5ddb2beb9
Andre Moraes <andre@amoraes.info>
2014-01-19 12:04:26 -0200

    static site

--- davd/main.go
+++ davd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/andrebq/exp/httpfs"
+	"github.com/andrebq/gas"
 	"log"
 	"net/http"
 )
@@ -12,19 +13,21 @@ var (
 	baseDir = flag.String("baseDir", ".", "Base dir to serve the content")
 )
 
-func index(w http.ResponseWriter, req *http.Request) {
-}
-
 func main() {
+	staticDir, err := gas.Abs("github.com/andrebq/exp/davd/site")
+	if err != nil {
+		log.Printf("Error loading static site directory. cause: %v", err)
+		return
+	}
 
 	m := &httpfs.Mount{BaseDir: *baseDir}
 	httpfs := httpfs.NewHttpFS(m, "/fs/")
 
 	http.Handle("/fs/", http.StripPrefix("/fs", httpfs))
-	http.HandleFunc("/", index)
+	http.Handle("/", http.FileServer(http.Dir(staticDir)))
 
 	log.Printf("Starting davd server at %v", *addr)
-	err := http.ListenAndServe(*addr, nil)
+	err = http.ListenAndServe(*addr, nil)
 	http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Printf("Error opening server: %v", err)
