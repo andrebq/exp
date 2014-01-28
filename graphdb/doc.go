6f987e160ea8795731ffbb2a543577f3f8b1881b
Andre Moraes <andre@amoraes.info>
2014-01-28 18:44:45 -0200

    removing old implementation

--- graphdb/doc.go
+++
@@ -1,4 +0,0 @@
-// Graphdb is a library to use a PostgreSQL database as a GraphDB.
-//
-// This is just a proof-of-concept.
-package graphdb
--- graphdb/pg_create_db.sql
+++
@@ -1,9 +0,0 @@
--- Database: graphdb_1
-
--- DROP DATABASE graphdb_1;
-
-CREATE DATABASE graphdb_1
-  WITH OWNER = graphdb
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
